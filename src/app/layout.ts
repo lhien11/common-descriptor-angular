fe1a299089f789f149de29f99a261f556b7d4101
lhien11@gmail.com <trunghien11>
2018-04-09 13:17:35 -0700

    Initial Commit with working project

---
+++ src/app/layout.ts
@@ -0,0 +1,28 @@
+const layout = [
+    { "type": "flex", "flex-flow": "row wrap", "items": [ "IncidentID", "PersonID" ]},
+    { "key": "Last Name", "title": "Last Name" },
+    { "key": "First Name", "title": "First Name" },
+    { "key": "Middle Name", "title": "Middle Name" },
+    { "key": "Date of Birth", "type": "date" },
+    { "key": "SSN", "title": "Social Security Number" },
+    { "key": "SID", "title": "State Identification Number" },
+    { "key": "FBI", "title": "FBI Number" }, 
+    { "type": "div",
+      "display": "flex",
+      "flex-direction": "row",
+      "items": [
+
+        { "key": "Gender.state", "flex": "1 1 50px",
+          "notitle": true, "placeholder": "Gender"
+        }
+      ]
+    },
+
+
+    
+]
+
+  
+  
+  export default layout;
+  
---
+++ src/app/schema.ts
@@ -0,0 +1,45 @@
+const schema = {
+  "type": "object",
+  "properties": {
+    "IncidentID": {
+      "type": "string"
+    },
+    "PersonID": {
+      "type": "string"
+    },
+    "Last Name": {
+      "type": "string",
+      "Pattern": "^([A-Z -,&amp;]{1,30})$"
+    },
+    "First Name": {
+      "type": "string"
+    },
+    "Middle Name": {
+      "type": "string"
+    },
+    "Date of Birth": {
+      "type": "string"
+    },
+    "SSN": { 
+      "type": "string" 
+    },
+    "SID": { 
+      "type": "string" 
+    },
+    "FBI": {
+      "type": "string"
+    },
+    "Gender": {
+      "type": "object",
+      "properties": {
+        "state": {
+          "type": "string",
+          "enum": [ "Male", "Female", "Unknown (Livescans)", "Unknown (Cardscans)" ]
+        }
+      }
+    },
+  }
+
+};
+
+export default schema;
